9b2627c8d5564728a7a5a36c2c86e6b6c1a6c8bf
d-saitou <laevateinn3918@gmail.com>
2021-09-19 00:25:23 +0900

    feat: Configure web screen and create login screen.

--- src/main/java/com/example/springboot/config/security/WebSecurityConfig.java
+++ src/main/java/com/example/springboot/config/security/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.springboot.config.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
@@ -9,6 +10,14 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
  */
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	/**
+	 * Configure WEB security.
+	 */
+	@Override
+	public void configure(WebSecurity web) {
+		web.ignoring().antMatchers("/webjars/**", "/js/**", "/css/**");
+	}
 
 	/**
 	 * Configure user authentication and authorization.
