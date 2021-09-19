980e894445fed76e04058c66a702b6ff75608a16
d-saitou <laevateinn3918@gmail.com>
2021-09-19 22:49:22 +0900

    chore: configure transaction advice.

--- src/main/java/com/example/springboot/config/AppConstants.java
+++ src/main/java/com/example/springboot/config/AppConstants.java
@@ -12,6 +12,23 @@ public class AppConstants {
 	/** configuration : session time out. */
 	public static final int DEFAULT_SESSION_TIMEOUT = 300;
 
+	// @formatter:off
+	/**
+	 * configuration : pointcut expression for transaction advice.
+	 * <ul>
+	 * <li>Package class of the hierarchy under "com.example.springmvc.domain.di.service".</li>
+	 * <li>Classes and methods for which Transactional annotation is not set.</li>
+	 * <li>The access modifier is public.</li>
+	 * <li>The method name prefix is "tx".</li>
+	 * </ul>
+	 */
+	public static final String TRANSACTION_POINTCUT_EXPRESSION =
+			"execution(public * com.example.springboot.domain.service..*.tx*(..)) and "
+			//+ "@target(org.springframework.stereotype.Service) and "
+			+ "not @annotation(org.springframework.transaction.annotation.Transactional) and "
+			+ "not @target(org.springframework.transaction.annotation.Transactional)";
+	// @formatter:on
+
 	/** security : URI to permit for all users. */
 	public static final List<String> URI_PERMIT_ALL;
 	static {
